7f3e5edd99006a66b01209dc36e5f95352c49fec
Patil Kundan <kundanpatil4828@gmail.com>
2026-07-10 13:52:29 +0530

    Completed User Registration API

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,43 @@
-// 
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
-const registerUser = (req, res) => {
-  res.json({
-    success: true,
-    message: "Register API Working",
-  });
+// Register User
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check existing user
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    // Encrypt password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Save user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User Registered Successfully",
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 module.exports = { registerUser };
